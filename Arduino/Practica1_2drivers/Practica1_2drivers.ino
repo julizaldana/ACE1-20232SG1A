ad24d7a0f2543d127708e019ee0a4fa50f11d81e
Jpanaza1906 <3848221850101@ingenieria.usac.edu.gt>
2023-08-14 22:04:44 -0600

    mensaje final

--- Arduino/Practica1_2drivers/Practica1_2drivers.ino
+++ Arduino/Practica1_2drivers/Practica1_2drivers.ino
@@ -4,6 +4,9 @@ LedControl matriz = LedControl(13,11,12,2);
 
 void setup() {
 	//  INICIALIZAR MATRIZ CON DRIVER
+  pinMode(53,INPUT);
+  pinMode(52,INPUT);
+  pinMode(10,INPUT);
 	matriz.shutdown(0, false);
   matriz.shutdown(1,false);
 	matriz.setIntensity(0, 8); 
@@ -48,73 +51,107 @@ byte estado[8][16] = { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, \
                        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, \
                        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, };
 
+//mostrar mensaje
+bool cambioDeModo = false;
+unsigned long tiempoInicio3Segundos= 0;
+bool botonPresionado = false;
+
+
+//modos de mensaje
 bool direccion = true; // true -> derecha | false -> izquierda
 bool modo = false; // true-> sin mov | false -> con mov
 int velocidad = 1;
 int potenciometro = 0;
+//funcion verificar
+
+//VALIDAR 
+void presiono3Segundo(){
+  if (digitalRead(10) == HIGH) {
+    if (!botonPresionado) {
+      tiempoInicio3Segundos = millis();  // Iniciar el temporizador al presionar el botón
+      botonPresionado = true;
+    }
+
+    if (millis() - tiempoInicio3Segundos >= 1500) {
+      // Realizar las acciones que desees aquí cuando el botón está presionado el tiempo deseado
+      cambioDeModo = true;
+      botonPresionado = false;
+    }
+  } else {
+    botonPresionado = false;  // Reiniciar el estado del botón si se suelta
+  }
+}
 
 void mostrarMensaje() {
-  if (digitalRead(53) == LOW) {
-    for (int i = 0; i < tamMensajeY; i++)
-      for (int j = 0; j < tamMensajeX; j++) {
-        if (mensaje[i][j] == 1) {
-          int residuo = (j + offset) % tamMensajeX;
-          pintarLed(residuo < 0 ? residuo + tamMensajeX : residuo, i);
-        }
-      }
-    //                   vv
-    // analogRead(A0);
-    // 0  - 1023 -- entrada análoga
-    // 1  -   15 -- velocidad
-    //                   ||
-    //                   ||
-    //                   vv
-    potenciometro = analogRead(A0);
-    velocidad = map(potenciometro, 0, 1024, 1, 15);    
-    for (int i = 0; i <= 10 + (2 * velocidad); i++) {
-        delay(20);
-    }
-    if (digitalRead(52) == LOW)
-      offset++;
-    else
-      offset--;    
-    
-    matriz.clearDisplay(0);
-    matriz.clearDisplay(1);
-    delay(10);
-  } else if (digitalRead(53) == HIGH) {
-    for (int k = 0; k < 28 && k >= 0;) {
-      if (digitalRead(52) == HIGH) {
-        if (k < 28)
-          k++;
-        else k=0;
-      } else {
-        if (k > 0)
-          k--;
-        else k=27;
-      }
-      for (int i = 0; i < 8; i++) {
-        for (int j = 6 * k; j < (6 * k) + 6; j++) {
+  //cambioDeModo = false;
+  while(!cambioDeModo){
+    if (digitalRead(53) == LOW) {
+      for (int i = 0; i < tamMensajeY; i++)
+        for (int j = 0; j < tamMensajeX; j++) {
           if (mensaje[i][j] == 1) {
-            pintarLed((j % 6) + 8, i);
+            int residuo = (j + offset) % tamMensajeX;
+            pintarLed(residuo < 0 ? residuo + tamMensajeX : residuo, i);
           }
         }
-      }
+      //                   vv
+      // analogRead(A0);
+      // 0  - 1023 -- entrada análoga
+      // 1  -   15 -- velocidad
+      //                   ||
+      //                   ||
+      //                   vv
       potenciometro = analogRead(A0);
-      velocidad = map(potenciometro, 0, 1024, 1, 15);
+      velocidad = map(potenciometro, 0, 1024, 1, 15);    
       for (int i = 0; i <= 10 + (2 * velocidad); i++) {
-        delay(20);
+        delay(15);
       }
-
-      if (digitalRead(53) == LOW)
-        break;
+      if (digitalRead(52) == LOW)
+        offset++;
+      else
+        offset--;    
       
       matriz.clearDisplay(0);
-      matriz.clearDisplay(1);
+      matriz.clearDisplay(1);      
+      presiono3Segundo();
+      if(cambioDeModo)
+          return;
+    } else if (digitalRead(53) == HIGH) {
+      for (int k = 0; k < 28 && k >= 0;) {
+        if (digitalRead(52) == HIGH) {
+          if (k < 28)
+            k++;
+          else k=0;
+        } else {
+          if (k > 0)
+            k--;
+          else k=27;
+        }
+        for (int i = 0; i < 8; i++) {
+          for (int j = 6 * k; j < (6 * k) + 6; j++) {
+            if (mensaje[i][j] == 1) {
+              pintarLed((j % 6) + 8, i);
+            }
+          }
+        }
+        potenciometro = analogRead(A0);
+        velocidad = map(potenciometro, 0, 1024, 1, 15);
+        for (int i = 0; i <= 10 + (2 * velocidad); i++) {
+          delay(15);
+        }
+        if (digitalRead(53) == LOW )
+          break;
+        matriz.clearDisplay(0);
+        matriz.clearDisplay(1);        
+        presiono3Segundo();
+        if(cambioDeModo)
+          return;
+      }
     }
   }
 }
 
 void loop() {
   mostrarMensaje();
+  
+
 }
